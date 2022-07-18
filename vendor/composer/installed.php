ac52df3b3e0fb749b69f9156c76897978d26af08
fawziyahfitwalla <109519079+fawziyahfitwalla@users.noreply.github.com>
2022-07-18 16:39:53 +0530

    Add files via upload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,34 @@
+<?php return array(
+    'root' => array(
+        'name' => 'fawzi/dmcc',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'fawzi/dmcc' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'simplehtmldom/simplehtmldom' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'b8d048e46b7f1964c28ea041d39ccb1d05f9a0ed',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../simplehtmldom/simplehtmldom',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+    ),
+);
